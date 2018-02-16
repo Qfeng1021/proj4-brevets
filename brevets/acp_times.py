21861cc8107552544e53496e45a6d774974ffcbf
qfeng <qfeng@uoregon.edu>
2018-02-16 15:52:37 -0800

    modified:   README.md 	modified:   brevets/Dockerfile 	modified:   brevets/acp_times.py 	modified:   brevets/credentials.ini 	modified:   brevets/flask_brevets.py 	modified:   brevets/templates/calc.html 	modified:   credentials-skel.ini

--- brevets/acp_times.py
+++ brevets/acp_times.py
@@ -5,7 +5,10 @@ following rules described at https://rusa.org/octime_alg.html
 and https://rusa.org/pages/rulesForRiders
 """
 import arrow
-
+min_speed = [15, 15, 11.428]
+max_speed = [34, 32, 30, 28]
+dist = [200, 400, 600, 1000]
+min_first_dist = max_speed[0] *min_speed[0] / (max_speed[0] - min_speed[0])
 #  Note for CIS 322 Fall 2016:
 #  You MUST provide the following two functions
 #  with these signatures, so that I can write
@@ -14,6 +17,23 @@ import arrow
 #  same arguments.  Arguments are explained in the
 #  javadoc comments.
 #
+def calculator(control_dist_km, brevet_dist_km, brevet_start_time, speed):
+    start = arrow.get(brevet_start_time, 'YYYY-MM-DD HH:mm')
+    i = 1
+    cost = 0
+    if control_dist_km > 200:
+        while min_first_dist <= control_dist_km <= brevet_dist_km:
+            if control_dist_km - dist[i] > 0:
+                cost = cost + round(((dist[i] - dist[i-1]) / speed[i]) * 60)
+                i = i + 1
+            else:
+                cost = cost + round(((control_dist_km - dist[i-1]) / speed[i]) * 60)
+                control_time = start.shift(minutes =+ cost).isoformat(sep = 'T')
+                return control_time
+    else:
+        cost = round((control_dist_km / speed[i-1]) * 60)
+        control_time = start.shift(minutes =+ cost).isoformat(sep = 'T')
+        return control_time
 
 
 def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
@@ -29,7 +49,13 @@ def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
        An ISO 8601 format date string indicating the control open time.
        This will be in the same time zone as the brevet start time.
     """
-    return arrow.now().isoformat()
+    start = arrow.get(brevet_start_time, 'YYYY-MM-DD HH:mm')
+    if control_dist_km == 0:
+        return start.isoformat(sep = 'T')
+    else:
+        return calculator(control_dist_km, brevet_dist_km, brevet_start_time, max_speed)
+
+
 
 
 def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
@@ -45,4 +71,10 @@ def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
        An ISO 8601 format date string indicating the control close time.
        This will be in the same time zone as the brevet start time.
     """
-    return arrow.now().isoformat()
+    start = arrow.get(brevet_start_time, 'YYYY-MM-DD HH:mm')
+    if control_dist_km == 0:
+        return start.shift(hours =+ 1).isoformat(sep = 'T')
+    elif control_dist_km == 200:
+        return start.shift(minutes =+ 13 * 60 + 30).isoformat(sep = 'T')
+    else:
+        return calculator(control_dist_km, brevet_dist_km, brevet_start_time, min_speed)
--- brevets/flask_brevets.py
+++ brevets/flask_brevets.py
@@ -7,10 +7,11 @@ Replacement for RUSA ACP brevet time calculator
 import flask
 from flask import request
 import arrow  # Replacement for datetime, based on moment.js
-import acp_times  # Brevet time calculations
+import acp_times # Brevet time calculations
 import config
-
+import time
 import logging
+import datetime
 
 ###
 # Globals
@@ -52,13 +53,18 @@ def _calc_times():
     Expects one URL-encoded argument, the number of miles.
     """
     app.logger.debug("Got a JSON request")
+    distance = request.args.get("distance", type = int)
+    begin_time = request.args.get("begin_time", type = str)
+    begin_date = request.args.get("begin_date", type = str)
+    brevet_start_time = begin_date + " " + begin_time
     km = request.args.get('km', 999, type=float)
+
     app.logger.debug("km={}".format(km))
     app.logger.debug("request.args: {}".format(request.args))
     # FIXME: These probably aren't the right open and close times
     # and brevets may be longer than 200km
-    open_time = acp_times.open_time(km, 200, arrow.now().isoformat)
-    close_time = acp_times.close_time(km, 200, arrow.now().isoformat)
+    open_time = acp_times.open_time(km, distance, brevet_start_time)
+    close_time = acp_times.close_time(km, distance, brevet_start_time)
     result = {"open": open_time, "close": close_time}
     return flask.jsonify(result=result)
 
